04b17aa9653e5a1526c770aeaa497d3559406fc7
Michael <43648081+michael@users.noreply.github.com>
2021-02-15 11:26:56 +0000

    Carousel work on writings

--- eportfolio/src/components/reflections/reflections.component.ts
+++ eportfolio/src/components/reflections/reflections.component.ts
@@ -11,11 +11,40 @@ import { Reflection } from 'src/data/reflection';
 export class ReflectionsComponent implements OnInit {
 
   @Input() item!: Reflection;
+  private currentIndex: number = -1;
+  loadFailed = false;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.item = <Reflection>this.dataService.selectedWriting;
+    this.currentIndex = this.dataService.writings.indexOf(this.item);
+
   }
 
+  onError(event: any){
+    console.log("markdown error occurred");
+    this.loadFailed = true;
+  }
+
+  onLoad(){
+    this.loadFailed = false;
+    console.log("markdown load finished");
+  }
+
+  navigateLeft(){
+    if (this.currentIndex>0){
+      this.loadFailed=false;
+      this.item = this.dataService.writings[this.currentIndex-1] as Reflection;
+      this.currentIndex = this.currentIndex - 1;
+    }
+  }
+
+  navigateRight(){
+    if (this.currentIndex<=this.dataService.writings.length-1){
+      this.loadFailed=false;
+      this.item = this.dataService.writings[this.currentIndex+1] as Reflection;
+      this.currentIndex = this.currentIndex + 1;
+    }
+  }
 }
