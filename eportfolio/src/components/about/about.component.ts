53e05da4585d6c9aed638e2387718b2f40abc5bc
Michael <43648081+michael@users.noreply.github.com>
2021-02-18 01:03:27 +0000

    Styling of About Me page

--- eportfolio/src/components/about/about.component.ts
+++ eportfolio/src/components/about/about.component.ts
@@ -10,6 +10,25 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let elm = document.querySelector(".about-profile");
+    let wpf = document.querySelector(".wallpaper-filter") as HTMLElement;
+
+    wpf.addEventListener("animationstart", ()=>{
+      console.log("WPF animation started");
+    });
+
+    wpf.addEventListener("animationend", (evt)=>{
+      console.log("WPF animation ended " + JSON.stringify(evt));
+    });
+
+    elm.addEventListener("mouseenter", ()=>{
+       wpf?.style.animation = 'fade-header-in .2s ease-in 0.1s 1 normal backwards';
+
+    });
+
+    elm.addEventListener("mouseleave", ()=>{
+      wpf?.style.animation = 'fade-header-out 0.2s ease-in 0.1s 1 normal forwards';
+    })
   }
 
 }
